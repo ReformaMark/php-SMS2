5b6758ee050019e5dd62e0d01e8987afd9601948
Mark <99254035+ReformaMark@users.noreply.github.com>
2024-09-26 22:58:15 +0800

    Init project with tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  relative: true,
+  content: [ 
+    './src/**/*.{html,js,php}',
+    './*.php'
+    
+  ],
+   theme: {
+     extend: {},
+},
+   plugins: [],
+}
