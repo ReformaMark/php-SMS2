c654f005bc397c2c886048a9795bd20c0d7de3b8
Mark <99254035+ReformaMark@users.noreply.github.com>
2024-09-27 13:55:00 +0800

    footer patter template

---
+++ public/templates/footer.php
@@ -0,0 +1,35 @@
+<footer class="flex justify-between px-10 py-5 bg-white mt-auto">
+    <div class="">
+    <h1 class="text-center">Logo</h1>
+    &copy; 2024 My Awesome Company. All rights reserved.
+    </div>
+   
+    <div class="">
+        <h1>Contact Information</h1>
+        <h3>Address</h3>
+        <h3>Phone number</h3>
+        <h3>Email address</h3>
+        <h3>Social media links</h3>
+    </div>
+    <div class="">
+        <h1>Contact Information</h1>
+        <h3>Address</h3>
+        <h3>Phone number</h3>
+        <h3>Email address</h3>
+        <h3>Social media links</h3>
+    </div>
+    <div class="">
+        <h1>Contact Information</h1>
+        <h3>Address</h3>
+        <h3>Phone number</h3>
+        <h3>Email address</h3>
+        <h3>Social media links</h3>
+    </div>
+    <div class="">
+        <h1>Contact Information</h1>
+        <h3>Address</h3>
+        <h3>Phone number</h3>
+        <h3>Email address</h3>
+        <h3>Social media links</h3>
+    </div>
+</footer>
