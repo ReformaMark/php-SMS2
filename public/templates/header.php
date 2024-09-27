3833dfbeee328c7a9c857ed670168d96ac9db133
Mark <99254035+ReformaMark@users.noreply.github.com>
2024-09-27 13:54:48 +0800

    Header pattern template

---
+++ public/templates/header.php
@@ -0,0 +1,15 @@
+<?php 
+
+?>
+
+<nav class="absolute flex justify-between items-center w-full bg-white px-10 py-5">
+    <div class="">
+        <h1>Logo</h1>
+    </div>
+    <ul class="flex justify-center gap-3">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Home</a></li>
+    </ul>
+</nav>
