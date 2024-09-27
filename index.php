158548c32a604774304c19718e2cc5a4b51cc425
Mark <99254035+ReformaMark@users.noreply.github.com>
2024-09-27 13:54:05 +0800

    add index.php as main page

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" 
+            content="width=device-width, initial-scale=1.0">
+    <link href="./output.css" rel="stylesheet" >
+</head>
+<body class="bg-gray-100 min-h-[80vh] flex flex-col justify-between">
+    
+    <?php include('public/templates/header.php') ?>
+    <div class="flex px-5 pb-10 pt-24 justify-center min-h-[100vh]">
+        <?php include('public/templates/login.php') ?>
+    </div>
+
+    <?php include('public/templates/footer.php') ?>
+   
+</body>
+</html>
