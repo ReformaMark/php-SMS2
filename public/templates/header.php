db2927adbc3e26beff794fb794b4170d5bc3e634
Mark <99254035+ReformaMark@users.noreply.github.com>
2024-09-28 10:40:26 +0800

    add condition to display the current username to nav

--- public/templates/header.php
+++ public/templates/header.php
@@ -1,15 +1,22 @@
 <?php 
-
+   
 ?>
 
 <nav class="absolute flex justify-between items-center w-full bg-white px-10 py-5">
     <div class="">
         <h1>Logo</h1>
     </div>
-    <ul class="flex justify-center gap-3">
-        <li><a href="#">Home</a></li>
-        <li><a href="#">Home</a></li>
-        <li><a href="#">Home</a></li>
-        <li><a href="#">Home</a></li>
-    </ul>
+    <?php 
+        if(isset($_SESSION['user_username'])){
+            echo '<p class=""> Welcome '. $_SESSION['user_username'] . '!</p>';
+        } else{
+            echo '<ul class="flex justify-center gap-3">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Home</a></li>
+                </ul>' ;
+        }
+    ?>
+    
 </nav>
