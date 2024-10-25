d2d68f74173569b580bbba0492e5080bc03b7237
Mark <99254035+ReformaMark@users.noreply.github.com>
2024-10-25 17:40:02 +0800

    a

--- src/config_session.php
+++ src/config_session.php
@@ -3,14 +3,6 @@
 ini_set('session.use_only_cookies', 1);
 ini_set('session.use_strict_mode', 1);
 
-session_set_cookie_params([
-    'lifetime' => 1800,
-    'domain' => "localhost",
-    'path' => '/',
-    'secure' => true,
-    'httponly' => true
-]);
-
 session_start();
 
 if (!isset($_SESSION["last_regeneration"])){
