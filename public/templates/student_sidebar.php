17563af5844e4b6aa6a8fa592883b4dcd27306a7
Mark <99254035+ReformaMark@users.noreply.github.com>
2024-10-25 19:39:04 +0800

    n

--- public/templates/student_sidebar.php
+++ public/templates/student_sidebar.php
@@ -1,4 +1,4 @@
-<section class="hidden lg:flex flex-col justify-between pb-5 shadow-md bg-white w-1/5 h-custom max-h-screen overflow-hidden">
+<section class="hidden md:flex flex-col justify-between pb-5 shadow-md bg-white w-1/5 h-custom max-h-screen overflow-hidden">
     <div class="w-full pt-3">
         <?php include($_SERVER['DOCUMENT_ROOT'] . BASE_URL . '/src/Views/student_sidebar_view.php') ?>
     </div>
