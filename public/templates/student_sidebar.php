91c4db5319162d8739fc68ab572b8041d999c9be
MarkR <99254035+ReformaMark@users.noreply.github.com>
2025-03-02 06:04:10 +0800

    fix: sidebar padding top

--- public/templates/student_sidebar.php
+++ public/templates/student_sidebar.php
@@ -2,7 +2,7 @@
     style="background-color: #212529;"
     class="hidden md:flex flex-col justify-between pb-5 text-white shadow-md w-1/5 h-custom max-h-screen overflow-hidden"
 >
-    <div class="w-full pt-3">
+    <div class="w-full pt-6">
         <?php include($_SERVER['DOCUMENT_ROOT'] . BASE_URL . '/src/Views/student_sidebar_view.php') ?>
     </div>
     <a href="<?php echo BASE_URL; ?>/src/logout.php" class="flex items-center w-full px-3 gap-x-3 mt-auto hover:bg-blue-500 hover:text-white transition-all duration-300 ease-linear font-normal text-lg p-2">
