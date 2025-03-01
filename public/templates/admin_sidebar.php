0d0eff09fca19fef325534fc8d924a7b604e507d
MarkR <99254035+ReformaMark@users.noreply.github.com>
2025-03-02 06:29:03 +0800

    students tab

--- public/templates/admin_sidebar.php
+++ public/templates/admin_sidebar.php
@@ -1,5 +1,5 @@
 <section class="hidden lg:flex flex-col justify-between pb-5 shadow-md bg-white w-1/5 h-custom max-h-screen overflow-hidden">
-    <div class="w-full pt-3">
+    <div class="w-full" style="padding-top: 4rem;">
         <?php include('../Views/admin_sidebar_view.php') ?>
         <!-- Admin-specific links -->
         <a href="../layouts/admin_dashboard.php" class="w-full flex items-center gap-x-3 pl-3 hover:bg-blue-500 hover:translate-x-5 hover:pl-5 hover:rounded-l-full hover:text-white transition-all duration-500 ease-linear font-normal text-lg border-b p-1">
--- public/templates/sidebar.php
+++ public/templates/sidebar.php
@@ -2,7 +2,7 @@
     style="background-color: #212529;"
     class="hidden lg:flex flex-col justify-between pb-5 shadow-md w-1/5 h-custom max-h-screen overflow-hidden text-white"
 >
-    <div class="w-full pt-3">
+    <div class="w-full" style="padding-top: 4rem;">
         
           
         <?php include('../Views/sidebar_view.php') ?>
