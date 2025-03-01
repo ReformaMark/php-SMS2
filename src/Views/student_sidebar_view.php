d663a0d665b3b0754fe35163d4694ce5ad818725
MarkR <99254035+ReformaMark@users.noreply.github.com>
2025-03-02 07:28:21 +0800

    .

--- src/Views/student_sidebar_view.php
+++ src/Views/student_sidebar_view.php
@@ -29,9 +29,9 @@ $coursesClass = (basename($_SERVER['PHP_SELF']) === 'student_ledger.php')
     ? 'pl-3 bg-blue-500 text-white rounded-l-full'
     : 'pl-3';
 
-echo '<div class="relative w-full">';
+echo '<div class="relative w-full text-white">';
 echo '<button id="coursesButton" aria-expanded="false" onclick="toggleAccordion()" class="px-3 cursor-pointer w-full flex justify-between items-center gap-x-3 transition-all duration-500 ease-linear font-normal text-lg p-1">
-        <div class="flex gap-x-3 items-center">        
+        <div class="flex gap-x-3 text-white items-center">        
             <svg class="size-5" height="32" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg"><path fill="white" d="M16 2C8.268 2 2 8.268 2 16s6.268 14 14 14 14-6.268 14-14S23.732 2 16 2zm0 26C9.373 28 4 22.627 4 16S9.373 4 16 4s12 5.373 12 12-5.373 12-12 12z"/><path d="M10 10h12v2H10zm0 4h12v2H10zm0 4h12v2H10z"/></svg>
             Courses
         </div>
