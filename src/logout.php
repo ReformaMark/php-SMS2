99fcd6b49cae056086ad0c4551d1c9917e5871f3
MarkR <99254035+ReformaMark@users.noreply.github.com>
2025-03-02 05:21:08 +0800

    fix: updated base url

--- src/logout.php
+++ src/logout.php
@@ -5,5 +5,5 @@ session_start();
 session_unset();
 session_destroy();
 
-header("Location: " . BASE_URL . "/login.php");
+header("Location: " . "schoolmanagementsystem2.com" . "/login.php");
 exit();
